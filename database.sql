4f55089420bddac9c03943ed19ec5d289ea4529e
Syco54645 <frank@splra.org>
2021-11-12 17:28:56 -0500

    Updated database dump with the newer database schema.

--- database.sql
+++ database.sql
@@ -1,5 +1,5 @@
 --
--- File generated with SQLiteStudio v3.3.3 on Sat Oct 30 19:28:28 2021
+-- File generated with SQLiteStudio v3.3.3 on Fri Nov 12 17:26:55 2021
 --
 -- Text encoding used: System
 --
@@ -10,7 +10,7 @@ BEGIN TRANSACTION;
 CREATE TABLE meta (
     meta_id    INTEGER       PRIMARY KEY AUTOINCREMENT,
     meta_key   VARCHAR (200),
-    meta_value VARCHAR (200) 
+    meta_value VARCHAR (200)
 );
 
 
@@ -22,7 +22,7 @@ CREATE TABLE show (
     city               VARCHAR (500),
     state              VARCHAR (100),
     date_last_modified INTEGER       DEFAULT (datetime('now') ),
-    artist             VARCHAR (50) 
+    artist             VARCHAR (50)
 );
 
 
@@ -33,7 +33,7 @@ CREATE TABLE show_song (
     song_id            INTEGER       REFERENCES song (song_id),
     track_number       DECIMAL,
     date_last_modified INTEGER       DEFAULT (datetime('now') ),
-    modifier           VARCHAR (200) 
+    modifier           VARCHAR (200)
 );
 
 
@@ -41,12 +41,71 @@ CREATE TABLE show_song (
 CREATE TABLE song (
     song_id            INTEGER       PRIMARY KEY AUTOINCREMENT,
     title              VARCHAR (500),
-    date_last_modified INTEGER       DEFAULT (datetime('now') ) 
+    date_last_modified INTEGER       DEFAULT (datetime('now') )
 );
+
+
+-- Trigger: SHOW_SONG_update_date_last_modified
+CREATE TRIGGER SHOW_SONG_update_date_last_modified
+         AFTER UPDATE OF show_id,
+                         song_id,
+                         track_number,
+                         modifier
+            ON show_song
+      FOR EACH ROW
+BEGIN
+    UPDATE show
+       SET date_last_modified = datetime('now')
+     WHERE show_song_id = NEW.show_song_id;
+END;
+
+
+-- Trigger: SHOW_update_date_last_modified
+CREATE TRIGGER SHOW_update_date_last_modified
+         AFTER UPDATE OF date,
+                         venue,
+                         city,
+                         state,
+                         artist
+            ON show
+      FOR EACH ROW
+BEGIN
+    UPDATE show
+       SET date_last_modified = datetime('now')
+     WHERE show_id = NEW.show_id;
+END;
+
+
+-- Trigger: SONG_update_date_last_modified
+CREATE TRIGGER SONG_update_date_last_modified
+         AFTER UPDATE OF title
+            ON song
+      FOR EACH ROW
+BEGIN
+    UPDATE song
+       SET date_last_modified = datetime('now')
+     WHERE song_id = OLD.song_id;
+END;
+
+
+-- View: database_last_modified
+CREATE VIEW database_last_modified AS
+    SELECT MAX(date)
+      FROM (
+               SELECT MAX(date_last_modified) AS date
+                 FROM song
+               UNION ALL
+               SELECT MAX(date_last_modified) AS date
+                 FROM show
+               UNION ALL
+               SELECT MAX(date_last_modified) AS date
+                 FROM show_song
+           );
 
 
 COMMIT TRANSACTION;
 PRAGMA foreign_keys = on;
+
 
 
 --
@@ -61,7 +120,7 @@ BEGIN TRANSACTION;
 CREATE TABLE meta (
     meta_id    INTEGER       PRIMARY KEY AUTOINCREMENT,
     meta_key   VARCHAR (200),
-    meta_value VARCHAR (200) 
+    meta_value VARCHAR (200)
 );
 
 INSERT INTO meta (
@@ -71,9 +130,22 @@ INSERT INTO meta (
                  )
                  VALUES (
                      1,
-                     'version',
+                     'schema_version',
                      '1.0.0'
                  );
+
+INSERT INTO meta (
+                     meta_id,
+                     meta_key,
+                     meta_value
+                 )
+                 VALUES (
+                     2,
+                     'database_repo',
+                     'https://github.com/Syco54645/TagBot.Database'
+                 );
+
+
 
 
 COMMIT TRANSACTION;
